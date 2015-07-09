5e73462c3c943cefb491143cf1b98c1b5043c19a
Juanan Pereira <juanan.pereira@ehu.es>
2015-07-09 11:37:41 +0200

    Objetuetara zuzendutako programazioa

---
+++ ozp/skriptak/disko.js
@@ -0,0 +1,13 @@
+
+	function Disko(izenburu, egile, salneurri, formatu) {
+		Produktu.apply(this, [izenburu, egile, salneurri];
+		this.formatu = function() {
+			console.log("Diskoaren formatua:" + formatu);
+		}
+	}
+
+	Disko.prototype = new Produktu();
+	Disco.prototype.constructor = Disco;
+
+
+
